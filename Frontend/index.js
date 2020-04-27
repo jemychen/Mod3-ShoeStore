eef4be13eb18608a714a5c6389cfe989537991d6
jemychen <jemychen@live.com>
2020-04-27 12:24:14 -0400

    added code but not working?

--- Frontend/index.js
+++ Frontend/index.js
@@ -1,1 +1,24 @@
 // Code your solution here
+fetch("http://localhost:3000/shoes")
+    .then(r => r.json())
+    .then(shoes => {
+        renderAllShoes(shoes)
+        renderDetail(shoes[0])
+    })
+
+function renderShoeSideBar(shoe) {
+    const shoesContainer = document.querySelector("#shoe-list")
+    const shoeLi = document.createElement("li")
+    shoeLi.className = "item"
+    shoeLi.dataset.id = shoe.id
+    shoeLi.textContent = shoe.name
+
+    shoeLi.addEventListener("click", e => {
+        renderDetail(shoe)
+    })
+    shoesContainer.append(shoeLi)
+}
+
+function renderAllShoes(shoes) {
+    shoes.forEach(shoe => renderShoeSideBar(shoe))
+}
